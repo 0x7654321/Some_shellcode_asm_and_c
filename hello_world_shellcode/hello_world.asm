c981c2f9b850894ff79ac8c27871f2820a3d2e3b
jesuisroot123 <65119790+jesuisroot123@users.noreply.github.com>
2022-03-28 09:40:07 +0200

    Add files via upload

---
+++ hello_world_shellcode/hello_world.asm
@@ -0,0 +1,14 @@
+SECTION .data
+msg db 'Hello world!!', 0Ah
+
+SECTION .text
+
+global _start
+
+_start:
+
+    mov edx, 14
+    mov ecx, msg
+    mov ebx, 1
+    mov eax, 4
+    int 80h
---
+++ hello_world_shellcode/hello_world_shellcode.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+unsigned char buffer[] = "\x66\xba\x0e\x00\x00\x00\x66\xb9\x1c\x00\x00\x00\x66\xbb\x01\x00\x00\x00\x66\xb8\x04\x00\x00\x00\xcd\x80\x00\x00\x48\x65\x6c\x6c\x6f\x20\x77\x6f\x72\x6c\x64\x21\x21\x0a";
+
+int main(int argc, char *argv[]) {
+    PVOID shellcode_exec = VirtualAlloc(0, sizeof(buffer), MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+    RtlCopyMemory(shellcode_exec, buffer, sizeof(buffer));
+    DWORD threadID;
+    HANDLE hThread = CreateThread(NULL, 0, (PTHREAD_START_ROUTINE)shellcode_exec, NULL, 0, &threadID);
+    WaitForSingleObject(hThread, INFINITE);
+}
