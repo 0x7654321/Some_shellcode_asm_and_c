58755983a627bdd4cd3747ead82eb5f7cdabd6b0
Cat-Man123 <65119790+Cat-Man123@users.noreply.github.com>
2022-05-16 15:39:13 +0200

    Update hello_world.asm

--- hello_world_shellcode/hello_world.asm
+++ hello_world_shellcode/hello_world.asm
@@ -2,7 +2,6 @@ SECTION .data
 msg db 'Hello world!!', 0Ah
 
 SECTION .text
-
 global _start
 
 _start:
@@ -11,4 +10,8 @@ _start:
     mov ecx, msg
     mov ebx, 1
     mov eax, 4
-    int 80h
+    int 0x80
+    
+    mov ebx, 0
+    mov eax, 1
+    int 0x80
